eeafda69a5c9ba9f7b41fca937925b54113220df
Robert <rob.moreno@hotmail.com>
2018-07-11 07:07:25 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Hello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bHello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bHello
+            // 
+            this.bHello.Location = new System.Drawing.Point(70, 97);
+            this.bHello.Name = "bHello";
+            this.bHello.Size = new System.Drawing.Size(75, 23);
+            this.bHello.TabIndex = 0;
+            this.bHello.Text = "Say Hello!";
+            this.bHello.UseVisualStyleBackColor = true;
+            this.bHello.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bHello_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.bHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bHello;
+    }
+}
+
